3c7fca7414fd572deed493d2ceb8ff908623bc58
Jeff Tao <jatao88@gmail.com>
2021-10-30 19:26:07 -0400

    WIP: Plug interface skeleton, port & connection store

--- src/Window.tsx
+++ src/Window.tsx
@@ -1,7 +1,113 @@
 /** @jsxImportSource @emotion/react */
 
 import { css } from "@emotion/react";
-import { PropsWithChildren, useState, useRef, useEffect } from "react";
+import {
+  PropsWithChildren,
+  useState,
+  useRef,
+  useEffect,
+  Fragment,
+} from "react";
+import { EmotionIcon } from "@emotion-icons/emotion-icon";
+import { Workflow, X, ChevronLeft, Circle } from "emotion-icons/octicons";
+
+function TitleButton({
+  extraCss,
+  onClick,
+  icon: Icon,
+  description,
+}: {
+  extraCss?: ReturnType<typeof css>;
+  icon: EmotionIcon;
+  description: string;
+  onClick: () => void;
+}) {
+  return (
+    <button
+      css={css`
+        width: 24px;
+        height: 24px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        color: white;
+        background: none;
+        border: 1px solid white;
+
+        cursor: pointer;
+
+        ${extraCss};
+      `}
+      onClick={onClick}
+      title={description}
+    >
+      <Icon />
+    </button>
+  );
+}
+
+function Frame({
+  title,
+  children,
+  onDragStart,
+}: PropsWithChildren<{
+  title: JSX.Element;
+  onDragStart: (offX: number, offY: number) => void;
+}>) {
+  return (
+    <div
+      css={css`
+        width: 100%;
+        height: 100%;
+
+        display: flex;
+        flex-direction: column;
+        overflow: hidden;
+
+        font-family: Iosevka;
+        border: 2px solid white;
+        border-radius: 4px;
+        background: black;
+      `}
+    >
+      <header
+        css={css`
+          width: 100%;
+          border-bottom: 2px solid white;
+          font-size: 20px;
+        `}
+        onMouseDown={(e) => onDragStart(e.clientX, e.clientY)}
+      >
+        {title}
+      </header>
+      <div
+        css={css`
+          flex-grow: 1;
+          flex-shrink: 1;
+          min-height: 0;
+        `}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
+
+const plugLine = css`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const plug = css`
+  width: 16px;
+  height: 16px;
+`;
+
+const plugName = css`
+  margin-left: 5px;
+`;
 
 export function Window({
   x,
@@ -23,104 +129,206 @@ export function Window({
   onClose: () => void;
   onFocus: () => void;
 }>) {
+  // Dragging
   const [dragging, setDragging] = useState(false);
-
   const onRepositionRef = useRef(onReposition);
   const setDraggingRef = useRef(setDragging);
-
   useEffect(() => {
     onRepositionRef.current = onReposition;
     setDraggingRef.current = setDragging;
   }, [onReposition, setDragging]);
 
+  const handleDrag = (mouseX: number, mouseY: number) => {
+    // References to setDragging, x, and y use the right closure
+    setDragging(true);
+
+    const offX = mouseX - x;
+    const offY = mouseY - y;
+
+    // But in these other callbacks, the closure will be stale,
+    // so we have to wrap them in refs
+
+    // We want dragging to be handled globally
+    // Otherwise, you can drag too quickly and get outside the title bar
+    const handleMouseMove = (e: MouseEvent) => {
+      onRepositionRef.current(e.clientX - offX, e.clientY - offY);
+    };
+    const handleMouseUp = () => {
+      setDraggingRef.current(false);
+
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseup", handleMouseUp);
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseup", handleMouseUp);
+  };
+
+  // Ports
+  const [showPorts, setShowPorts] = useState(false);
+
   return (
-    <div
-      css={css`
-        position: absolute;
-        left: ${x}px;
-        top: ${y}px;
-        z-index: ${z};
-        width: ${w}px;
-        height: ${h}px;
-
-        display: flex;
-        flex-direction: column;
-        overflow: hidden;
-
-        font-family: Iosevka;
-        border: 2px solid white;
-        border-radius: 4px;
-        background: black;
-
-        cursor: ${dragging ? "grabbing" : "auto"};
-      `}
-      onMouseDown={onFocus}
-    >
-      <header
-        css={css`
-          width: 100%;
-          border-bottom: 2px solid white;
-          font-size: 20px;
-          padding: 4px 8px;
-
-          display: flex;
-          flex-direction: row;
-          justify-content: space-between;
-          align-items: center;
-
-          cursor: ${dragging ? "grabbing" : "grab"};
-        `}
-        onMouseDown={(e) => {
-          setDragging(true);
-
-          const offX = e.clientX - x;
-          const offY = e.clientY - y;
-
-          // We want dragging to be handled globally
-          // Otherwise, you can drag too quickly and get outside the title bar
-          const handleMouseMove = (e: MouseEvent) => {
-            onRepositionRef.current(e.clientX - offX, e.clientY - offY);
-          };
-          const handleMouseUp = () => {
-            setDraggingRef.current(false);
-
-            document.removeEventListener("mousemove", handleMouseMove);
-            document.removeEventListener("mouseup", handleMouseUp);
-          };
-
-          document.addEventListener("mousemove", handleMouseMove);
-          document.addEventListener("mouseup", handleMouseUp);
-        }}
-      >
-        Window
-        <nav>
-          <button
-            css={css`
-              width: 20px;
-              height: 20px;
-              display: flex;
-              justify-content: center;
-              align-items: center;
-
-              color: white;
-              background: none;
-              border: 2px solid white;
-            `}
-            onClick={onClose}
-          >
-            X
-          </button>
-        </nav>
-      </header>
+    <Fragment>
       <div
         css={css`
-          flex-grow: 1;
-          flex-shrink: 1;
-          min-height: 0;
+          position: absolute;
+          left: ${x}px;
+          top: ${y}px;
+          z-index: ${z};
+          width: ${w}px;
+          height: ${h}px;
+
+          cursor: ${dragging ? "grabbing" : "auto"};
         `}
+        onMouseDown={onFocus}
       >
-        {children}
+        <Frame
+          title={
+            <div
+              css={css`
+                display: flex;
+                flex-direction: row;
+                justify-content: space-between;
+                align-items: center;
+                padding: 4px 8px;
+                cursor: ${dragging ? "grabbing" : "grab"};
+              `}
+            >
+              <span>Window</span>
+              <nav
+                css={css`
+                  display: flex;
+                  flex-direction: row;
+                `}
+              >
+                <TitleButton
+                  extraCss={css`
+                    padding: 4px;
+                  `}
+                  description="Toggle ports panel"
+                  icon={Workflow}
+                  onClick={() => setShowPorts(!showPorts)}
+                />
+                <TitleButton
+                  extraCss={css`
+                    margin-left: 5px;
+                  `}
+                  description="Close window"
+                  icon={X}
+                  onClick={onClose}
+                />
+              </nav>
+            </div>
+          }
+          onDragStart={handleDrag}
+        >
+          {children}
+        </Frame>
       </div>
-    </div>
+      {showPorts && (
+        <div
+          css={css`
+            position: absolute;
+            left: ${x + w}px;
+            top: ${y}px;
+            z-index: ${z};
+            min-width: 100px;
+            min-height: 100px;
+            max-width: ${w}px;
+            max-height: ${h}px;
+          `}
+          onMouseDown={onFocus}
+        >
+          <Frame
+            title={
+              <div
+                css={css`
+                  display: flex;
+                  flex-direction: row;
+                  align-items: center;
+                  width: 100%;
+                  height: 100%;
+                  padding: 4px 8px;
+                  cursor: ${dragging ? "grabbing" : "grab"};
+                `}
+              >
+                <TitleButton
+                  extraCss={css`
+                    margin-right: 5px;
+                  `}
+                  icon={ChevronLeft}
+                  description="Close ports panel"
+                  onClick={() => setShowPorts(false)}
+                />
+                Ports
+              </div>
+            }
+            onDragStart={handleDrag}
+          >
+            <div
+              css={css`
+                padding: 8px;
+              `}
+            >
+              <h1
+                css={css`
+                  font-size: 20px;
+                  font-weight: normal;
+                `}
+              >
+                Inputs
+              </h1>
+              <hr
+                css={css`
+                  margin-bottom: 5px;
+                `}
+              />
+              <ul
+                css={css`
+                  list-style: none;
+                `}
+              >
+                <li css={plugLine}>
+                  <Circle css={plug} />
+                  <span css={plugName}>X</span>
+                </li>
+                <li css={plugLine}>
+                  <Circle css={plug} />
+                  <span css={plugName}>Y</span>
+                </li>
+              </ul>
+              <h1
+                css={css`
+                  font-size: 20px;
+                  font-weight: normal;
+                  margin-top: 10px;
+                `}
+              >
+                Outputs
+              </h1>
+              <hr
+                css={css`
+                  margin-bottom: 5px;
+                `}
+              />
+              <ul
+                css={css`
+                  list-style: none;
+                `}
+              >
+                <li css={plugLine}>
+                  <Circle css={plug} />
+                  <span css={plugName}>X</span>
+                </li>
+                <li css={plugLine}>
+                  <Circle css={plug} />
+                  <span css={plugName}>Y</span>
+                </li>
+              </ul>
+            </div>
+          </Frame>
+        </div>
+      )}
+    </Fragment>
   );
 }
---
+++ src/ports/ports.ts
@@ -0,0 +1,140 @@
+import { Observable, Observer, Subject, Subscription } from "rxjs";
+import { types, SnapshotOrInstance } from "mobx-state-tree";
+import { observable } from "mobx";
+
+export function portID(windowName: string, portName: string) {
+  return `${windowName}:${portName}`;
+}
+
+const PortConfig = types.model({
+  id: types.identifier,
+  type: types.enumeration(["number"]),
+});
+
+export type IPortConfig = SnapshotOrInstance<typeof PortConfig>;
+
+const Connection = types.model({
+  from: types.reference(PortConfig),
+  to: types.reference(PortConfig),
+});
+
+export const Ports = types
+  .model({
+    inputs: types.map(PortConfig),
+    outputs: types.map(PortConfig),
+    connections: types.array(Connection),
+  })
+  .volatile(() => ({
+    // Port ID -> Subject
+    toStreams: observable.map<string, Observer<unknown>>({}),
+    fromStreams: observable.map<string, Observable<unknown>>({}),
+  }))
+  .views((self) => {
+    function connectionsTo(portID: string) {
+      return self.connections
+        .filter((connection) => connection.to.id === portID)
+        .map((connection) => connection.to);
+    }
+
+    function connectionsFrom(portID: string) {
+      return self.connections
+        .filter((connection) => connection.from.id === portID)
+        .map((connection) => connection.from);
+    }
+
+    return {
+      connectionsTo,
+      connectionsFrom,
+    };
+  })
+  .actions((self) => {
+    const subscriptions: { [connectionID: string]: Subscription } = {};
+
+    function connectionID(from: string, to: string) {
+      return `${from}->${to}}`;
+    }
+
+    function plug(from: string, to: string) {
+      const from$ = self.fromStreams.get(from);
+      const to$ = self.toStreams.get(to);
+      if (!from$ || !to$) {
+        throw new Error(`Cannot connect ${from} -> ${to}`);
+      }
+
+      self.connections.push({ from, to });
+      subscriptions[connectionID(from, to)] = from$.subscribe(to$);
+    }
+
+    function unplug(from: string, to: string) {
+      const id = connectionID(from, to);
+      if (!(id in subscriptions)) {
+        throw new Error(`Cannot disconnect ${from} -> ${to}`);
+      }
+
+      self.connections.splice(
+        self.connections.findIndex(
+          (connection) => connection.from.id === from && connection.to.id === to
+        ),
+        1
+      );
+      subscriptions[id].unsubscribe();
+      delete subscriptions[id];
+    }
+
+    function registerInput(portConfig: SnapshotOrInstance<typeof PortConfig>) {
+      if (self.inputs.has(portConfig.id)) {
+        throw new Error(
+          `Cannot re-register existing input port ${portConfig.id}`
+        );
+      }
+
+      self.inputs.set(portConfig.id, portConfig);
+      self.toStreams.set(portConfig.id, new Subject<unknown>());
+    }
+
+    function unregisterInput(toPort: string) {
+      if (!self.inputs.has(toPort)) {
+        throw new Error(`Cannot unregister non-existent input port ${toPort}`);
+      }
+
+      self.inputs.delete(toPort);
+      self
+        .connectionsTo(toPort)
+        .forEach((fromPort) => unplug(fromPort.id, toPort));
+      self.toStreams.delete(toPort);
+    }
+
+    function registerOutput(portConfig: SnapshotOrInstance<typeof PortConfig>) {
+      if (self.outputs.has(portConfig.id)) {
+        throw new Error(
+          `Cannot re-register existing output port ${portConfig.id}`
+        );
+      }
+
+      self.outputs.set(portConfig.id, portConfig);
+      self.fromStreams.set(portConfig.id, new Subject<unknown>());
+    }
+
+    function unregisterOutput(fromPort: string) {
+      if (!self.outputs.has(fromPort)) {
+        throw new Error(`Cannot re-register existing output port ${fromPort}`);
+      }
+
+      self.outputs.delete(fromPort);
+      self
+        .connectionsFrom(fromPort)
+        .forEach((toPort) => unplug(fromPort, toPort.id));
+      self.fromStreams.delete(fromPort);
+    }
+
+    return {
+      plug,
+      unplug,
+      registerInput,
+      unregisterInput,
+      registerOutput,
+      unregisterOutput,
+    };
+  });
+
+export const portStore = Ports.create();
